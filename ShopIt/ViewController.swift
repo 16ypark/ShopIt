b05b5aa08951a7009ee1d984ff5e463e18c93191
Yoo Hwa Park <16ypark@kaist.ac.kr>
2020-04-08 15:58:53 +0900

    allowed adding items to shopping list

--- ShopIt/ViewController.swift
+++ ShopIt/ViewController.swift
@@ -9,10 +9,41 @@
 import UIKit
 
 class ViewController: UITableViewController {
-
+    var shoppingItems = [String]()
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(addItem))
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return shoppingItems.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Item", for: indexPath)
+        cell.textLabel?.text = shoppingItems[indexPath.row]
+        return cell
+    }
+    
+    @objc func addItem() {
+        let ac = UIAlertController(title: "Enter item to shopping list", message: nil, preferredStyle: .alert)
+        ac.addTextField()
+
+        let submitAction = UIAlertAction(title: "Submit", style: .default) { [weak self, weak ac] action in
+            guard let answer = ac?.textFields?[0].text else { return }
+            self?.submit(answer)
+        }
+
+        ac.addAction(submitAction)
+        present(ac, animated: true)
+    }
+    
+    func submit(_ answer: String) {
+        shoppingItems.insert(answer, at: 0)
+
+        let indexPath = IndexPath(row: 0, section: 0)
+        tableView.insertRows(at: [indexPath], with: .automatic)
     }
 
 
