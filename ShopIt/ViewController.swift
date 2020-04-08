8fc8ac51bfc9431871a3924743f6ebbcd3080dc3
Yoo Hwa Park <16ypark@kaist.ac.kr>
2020-04-08 15:10:15 +0900

    first commit

---
+++ ShopIt/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ShopIt
+//
+//  Created by Yoo Hwa Park on 2020/04/08.
+//  Copyright © 2020 Yoo Hwa Park. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
